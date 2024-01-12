0f999a972555f0719fed186e693e11e00656c16a
c010703m <115143803+c010703m@users.noreply.github.com>
2024-01-12 18:44:58 +0000

    Started spline roads, they work but i want to be able to have muitiple splines

---
+++ Advanced Mechanics Design/Assets/Spline Road.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineRoad : MonoBehaviour
+{
+    [SerializeField]
+    private int resolution;
+
+    private SplineSampler m_splineSampler;
+    private MeshFilter m_meshFilter;
+
+    private List<Vector3> m_vertsP1;
+    private List<Vector3> m_vertsP2;
+
+    private void Start()
+    {
+        m_splineSampler = GetComponent<SplineSampler>();
+        m_meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Update()
+    {
+        GetVerts();
+        BuildMesh();
+    }
+
+    private void GetVerts()
+    {
+        m_vertsP1 = new List<Vector3>();
+        m_vertsP2 = new List<Vector3>();
+
+        float step = 1f / (float)resolution;
+        for (int i = 0; i < resolution; i++)
+        {
+            float t = step * i;
+            m_splineSampler.SampleSplineWidth(t, out Vector3 p1, out Vector3 p2);
+            m_vertsP1.Add(p1);
+            m_vertsP2.Add(p2);
+        }
+    }
+
+    private void BuildMesh()
+    {
+        Mesh m = new Mesh();
+        List<Vector3> verts = new List<Vector3>();
+        List<int> tris = new List<int>();
+        int offset = 0;
+
+        int length = m_vertsP2.Count;
+
+        for (int i = 1; i <= length; i++)
+        {
+            Vector3 p1 = m_vertsP1[i-1];
+            Vector3 p2 = m_vertsP2[i-1];
+            Vector3 p3;
+            Vector3 p4;
+
+            if (i == length)
+            {
+                p3 = m_vertsP1[0];
+                p4 = m_vertsP2[0];
+            }
+            else
+            {
+                p3 = m_vertsP1[i];
+                p4 = m_vertsP2[i];
+            }
+
+            offset = 4 * (i - 1);
+
+            int t1 = offset + 0;
+            int t2 = offset + 2;
+            int t3 = offset + 3;
+
+            int t4 = offset + 3;
+            int t5 = offset + 1;
+            int t6 = offset + 0;
+
+            verts.AddRange(new List<Vector3> { p1, p2, p3, p4 });
+            tris.AddRange(new List<int> { t1, t2, t3, t4, t5, t6 });
+        }
+
+        m.SetVertices(verts);
+        m.SetTriangles(tris, 0);
+        m_meshFilter.mesh = m;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_vertsP1 != null && m_vertsP2 != null)
+        {
+            Gizmos.color = Color.green;
+
+            for (int i = 0; i < m_vertsP1.Count; i++)
+            {
+                //Gizmos.DrawSphere(m_vertsP1[i], 1f);
+                Handles.SphereHandleCap(0, m_vertsP1[i] , quaternion.identity, 1f, EventType.Repaint);
+                Handles.SphereHandleCap(0, m_vertsP2[i], quaternion.identity, 1f, EventType.Repaint);
+                //Gizmos.DrawSphere(m_vertsP2[i], 1f);
+
+                Gizmos.DrawLine(m_vertsP1[i], m_vertsP2[i]);
+            }
+        }
+    }
+}
---
+++ Advanced Mechanics Design/Assets/SplineSampler.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Splines;
+
+[ExecuteInEditMode()]
+public class SplineSampler : MonoBehaviour
+{
+    [SerializeField]
+    private SplineContainer m_splineContainer;
+    [SerializeField]
+    private int m_splineIndex;
+    [SerializeField]
+    public int NumSplines;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float m_time;
+    [SerializeField]
+    public float m_width = 1f;
+
+    float3 position;
+    float3 tangent;
+    float3 upVector;
+
+    float3 forward;
+
+    float3 p1;
+    float3 p2;
+
+    private void Update()
+    {
+        if (m_splineContainer != null)
+        {
+            m_splineContainer.Evaluate(m_splineIndex, m_time, out position, out forward, out upVector);
+            float3 right = Vector3.Cross(forward, upVector).normalized;
+            p1 = position + (right * m_width);
+            p2 = position + (-right * m_width);
+        }
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Handles.SphereHandleCap(0, p1, quaternion.identity, 1f, EventType.Repaint);
+        Handles.SphereHandleCap(0, p2, quaternion.identity, 1f, EventType.Repaint);
+
+        Gizmos.DrawLine(p1, p2);
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawSphere(p1, 1f);
+
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawSphere(p2, 1f);
+    }
+
+    public void SampleSplineWidth(float t, out Vector3 sampledP1, out Vector3 sampledP2)
+    {
+        if (m_splineContainer != null)
+        {
+            m_splineContainer.Evaluate(m_splineIndex, t, out position, out forward, out upVector);
+
+            float3 right = Vector3.Cross(forward, upVector).normalized;
+            sampledP1 = position + (right * m_width);
+            sampledP2 = position + (-right * m_width);
+        }
+        else
+        {
+            sampledP1 = float3.zero;
+            sampledP2 = float3.zero;
+        }
+
+
+    }
+}
