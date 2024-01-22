0e316d8a91145fd6d95544319b62b5a4b60e9a74
c010703m <115143803+c010703m@users.noreply.github.com>
2024-01-22 17:18:30 +0000

    Tried adding turret rotate, barrel doesn't work

--- Advanced Mechanics Design/Assets/02-TankController/Scripts/Turret.cs
+++ Advanced Mechanics Design/Assets/02-TankController/Scripts/Turret.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Graphs;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Turret : MonoBehaviour
 {
-	[SerializeField] private Transform m_CameraMount;
+	[SerializeField] private Camera m_Camera;
 	[SerializeField] private Transform m_Turret;
 	[SerializeField] private Transform m_Barrel;
+	[SerializeField] private float m_RotationSpeed;
+    [SerializeField] private float m_VerticalRotationSpeed;
+	[SerializeField] private float m_MinVerticalAngle = -30f;
+	[SerializeField] private float m_MaxVerticalAngle = 30f;
 
-	private TankSO m_Data;
+    private TankSO m_Data;
 	private bool m_RotationDirty;
 	private Coroutine m_CRAimingTurret;
 
@@ -20,16 +25,66 @@ public class Turret : MonoBehaviour
 
 	public void Init(TankSO inData)
 	{
-		
+		m_Data = inData;
 	}
 
-	public void SetRotationDirty()
+    private void Start()
+    {
+    }
+
+    public void SetRotationDirty()
 	{
-		
+		m_RotationDirty = true;
 	}
 
-	private IEnumerator C_AimTurret()
+    private void Update()
+    {
+        //Vector3 camFwdVec = m_Camera.transform.forward;
+
+        //Vector3 projectedVec = Vector3.ProjectOnPlane(camFwdVec, transform.parent.up);
+
+        //Quaternion targetRot = Quaternion.LookRotation(projectedVec, transform.parent.up);
+
+        //Debug.DrawLine(transform.position, transform.position + projectedVec * 50, Color.blue);
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, m_rotationSpeed * Time.deltaTime);
+		if(m_RotationDirty)
+		{
+			if(m_CRAimingTurret != null)
+			{
+				StopCoroutine(m_CRAimingTurret);
+			}
+
+			m_CRAimingTurret = StartCoroutine(C_AimTurret());
+		}
+    }
+
+    private IEnumerator C_AimTurret()
 	{
-		yield return null;
+        Vector3 camFwdVec = m_Camera.transform.forward;
+		Vector3 camUpVec = m_Camera.transform.forward;
+
+		camFwdVec.y = 0f;
+		camUpVec.z = 0f;
+
+        Quaternion targetRot = Quaternion.LookRotation(camFwdVec);
+		Quaternion barrelTargetRot = Quaternion.LookRotation(camUpVec);
+
+		m_Turret.rotation = Quaternion.Slerp(m_Turret.rotation, targetRot, m_RotationSpeed * Time.deltaTime);
+		
+		m_Barrel.rotation = Quaternion.Slerp(m_Barrel.rotation, barrelTargetRot, m_VerticalRotationSpeed * Time.deltaTime);
+ 
+
+        //	Vector3 camFwdVec = m_CameraMount.forward;
+
+        //	Vector3 projectedVec = Vector3.ProjectOnPlane(camFwdVec, transform.parent.up);
+
+        //	Quaternion targetRot = Quaternion.LookRotation(projectedVec, transform.parent.up);
+
+        //	Debug.DrawLine(transform.position, transform.position + projectedVec * 50, Color.blue);
+
+        //	transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, m_Data.TurretData.TurretTraverseSpeed * Time.deltaTime);
+
+        yield return null;
 	}
 }
